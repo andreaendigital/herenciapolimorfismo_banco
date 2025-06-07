229efe03e268a73350bb3545a6d91154768bd8db
Andrea Rosero <andrea.rosero.p@gmail.com>
2025-06-07 18:52:07 -0400

    creacion clase cliente en paquete clientes

--- Bank_Europe/src/bank_europe/interfaces/InfoCliente.java
+++ Bank_Europe/src/bank_europe/interfaces/InfoCliente.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package bank_europe;
+package bank_europe.interfaces;
 
 /**
  *
  * @author Andrea
  */
 public interface InfoCliente {
-    
+    String mostrarInformacionCliente();
 }
